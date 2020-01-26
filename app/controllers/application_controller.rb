ad1e4588adde67c5befe9c7b0fc0458bf803f069
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-26 21:41:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,20 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+    get '/' do
+    erb :index
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
+
 
 end
